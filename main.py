563bccb28532b87a2cd2215b8a6f72eb8acc2f68
Saj <87875257+SajHere@users.noreply.github.com>
2022-01-07 16:18:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ async def on_ready():
     print(f'With ID: {client.user.id}')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'discord.gg/V5K6GsBvpJ | . help'))
 
-# Load cogs
+# Loads your other files (cogs)
 extensions = [ "cogs.help","cogs.play", "cogs.Islam","cogs.meme", "cogs.info", "cogs.Hidden", "cogs.Moderation", "cogs.purge" ]
-
+# prints them
 print(extensions)
 
+# a command thingy to load them dw about it
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
@@ -27,6 +28,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-
-client.run("ODk5NzMzMzY2MzI3MzQ1MTgy.YW3D_g.rkzoA31nHEeFgWArayZ66XcKfS0")
+#runs your code onto the bot with the token provided
+client.run("YOUR_TOKEN")
 
